d66c1c9ce8c8037712b96f0d69caf7c912eda1a4
Matthew Waddell <matt@littlecraft.io>
2016-07-29 09:55:35 -0700

    Moved ofono ag attachement sequence into state machine

--- src/phony/bluetooth/profiles/handsfree/ofono.py
+++ src/phony/bluetooth/profiles/handsfree/ofono.py
@@ -5,6 +5,57 @@ import glib
 
 from phony.base.log import ClassLogger, ScopedLogger, Levels
 
+class State:
+  START = 0
+  BEGIN_ATTACH = 1
+  WAITING_FOR_MODEM_ONLINE = 2
+  WAITING_FOR_MODEM_APPEARANCE = 3
+
+  __state = START
+
+  audio_gateway_ready_listener = None
+  modem = None
+  adapter = None
+  device = None
+
+  def __init__(self, last = None, next_state = START):
+    if last:
+      self.audio_gateway_ready_listener = last.audio_gateway_ready_listener
+      self.modem = last.modem
+      self.adapter = last.adapter
+      self.device = last.device
+
+    self.__state = next_state
+
+  def waiting_for_modem_online(self):
+    return self.__state == State.WAITING_FOR_MODEM_ONLINE
+
+  def waiting_for_modem_appearance(self):
+    return self.__state == State.WAITING_FOR_MODEM_APPEARANCE
+
+  @staticmethod
+  def empty():
+    return State()
+
+  @staticmethod
+  def begin_attach(last_state, audio_gateway_ready_listener):
+    new_state = State(last_state, State.BEGIN_ATTACH)
+    new_state.audio_gateway_ready_listener = audio_gateway_ready_listener
+    return new_state
+
+  @staticmethod
+  def modem_found(last_state, modem):
+    new_state = State(last_state, State.WAITING_FOR_MODEM_ONLINE)
+    new_state.modem = modem
+    return new_state
+
+  @staticmethod
+  def modem_not_found(last_state, adapter, device):
+    new_state = State(last_state, State.WAITING_FOR_MODEM_APPEARANCE)
+    new_state.device = device
+    new_state.adapter = adapter
+    return new_state
+
 class Ofono(ClassLogger):
   SERVICE_NAME = 'org.ofono'
   MANAGER_INTERFACE = 'org.ofono.Manager'
@@ -15,12 +66,7 @@ class Ofono(ClassLogger):
   __bus = None
   __manager = None
 
-  __modem = None
-
-  __adapter = None
-  __device = None
-
-  __found_hfp_audio_gateway_listener = None
+  __state = State.empty()
 
   def __init__(self, bus):
     ClassLogger.__init__(self)
@@ -34,95 +80,87 @@ class Ofono(ClassLogger):
       self.MANAGER_INTERFACE
     )
 
-    self.__manager.connect_to_signal('ModemAdded', self.modem_added)
-    self.__manager.connect_to_signal('ModemRemoved', self.modem_removed)
+    self.__manager.connect_to_signal('ModemAdded', self.__modem_found)
+    self.__manager.connect_to_signal('ModemRemoved', self.__modem_removed)
 
   @ClassLogger.TraceAs.call()
   def stop(self):
-    pass
+    self.__state = State.empty()
 
   @ClassLogger.TraceAs.call()
-  def attach_audio_gateway(self, adapter, device, listener):
-    self.__modem = None
-    self.__found_hfp_audio_gateway_listener = None
+  def attach_audio_gateway(self, adapter, device, audio_gateway_ready_listener):
 
-    self.__adapter = adapter
-    self.__device = device
+    self.__state = State.begin_attach(self.__state, audio_gateway_ready_listener)
 
-    path, properties = self.__find_our_hfp_modem()
-
-    if path and properties['Online']:
-      # It's already available, so use it and return
-
-      self.log().debug('Found modem immediately!')
-
-      self.__show_modem_properties(properties)
-      listener(OfonoHfpAg(path, self.__bus))
-      return
+    path, properties = self.__find_our_hfp_modem(adapter, device)
 
     if path:
-      # If one was found, but it is not yet online,
-      # wait for it to go online.
-
-      self.log().debug('Found modem, but it is offline, waiting for it to come online')
-
-      self.__modem = dbus.Interface(
+      modem = dbus.Interface(
         self.__bus.get_object(self.SERVICE_NAME, path),
         self.MODEM_INTERFACE
       )
 
-      self.__modem.connect_to_signal('PropertyChanged',
-        self.wait_for_modem_to_go_online)
-    else:
-      # Otherwise, wait for the modem to be attached
-      # to the adapter and device.
+      self.__state = State.modem_found(self.__state, modem)
 
+      if properties['Online']:
+        self.log().debug('Found modem immediately!')
+
+        self.__modem_is_online()
+      else:
+        self.log().debug('Found modem, but it is offline, waiting for it to come online')
+
+        modem.connect_to_signal('PropertyChanged',
+          self.__wait_for_modem_online)
+
+    else:
       self.log().debug('No modem found, waiting for one to appear')
 
-    self.__found_hfp_audio_gateway_listener = listener
+      self.__state = State.modem_not_found(self.__state, adapter, device)
 
   @ClassLogger.TraceAs.event()
-  def modem_added(self, path, properties):
+  def __modem_found(self, path, properties):
     pass
 
   @ClassLogger.TraceAs.event()
-  def modem_removed(self, path):
+  def __modem_removed(self, path):
     pass
 
-  def wait_for_modem_to_go_online(self, name, value):
-    if self.__modem:
+  def __wait_for_modem_online(self, name, value):
+    if self.__state.waiting_for_modem_online():
       if name == 'Online' and value:
-        self.log().debug('Modem is online, notifying...')
-        ag = OfonoHfpAg(self.__modem.object_path, self.__bus)
-        self.__found_hfp_audio_gateway_listener(ag)
+        self.__modem_is_online()
 
-        self.__modem = None
-        self.__adapter = None
-        self.__device = None
+  def __modem_is_online(self):
+    self.log().debug('Modem is online, notifying...')
 
+    ag = OfonoHfpAg(self.__state.modem.object_path, self.__bus)
+    self.__state.audio_gateway_ready_listener(ag)
 
-  def __find_our_hfp_modem(self):
+    self.__state = State.empty()
+
+  def __find_our_hfp_modem(self, adapter, device):
     found_path = None
     found_properties = None
 
     modems = self.__manager.GetModems()
 
-    # TODO: Verify that the modem is attached to _both_
-    # the adapter and remote device.  This is necessary
-    # in cases where the host has more than one BT adapter.
-
     for path, properties in modems:
-      if self.__is_our_hfp_modem(path, properties):
+      if Ofono.__is_our_hfp_modem(adapter, device, path, properties):
         found_path = path
         found_properties = properties
         break
 
     return (found_path, found_properties)
 
-  def __is_our_hfp_modem(self, path, properties):
+  @staticmethod
+  def __is_our_hfp_modem(adapter, device, path, properties):
+    # TODO: Verify that the modem is attached to _both_
+    # the adapter and remote device.  This is necessary
+    # in cases where the host has more than one BT adapter.
+
     path = path.replace('_', ':')
     path = path.upper()
-    return path.endswith(self.__device.address()) \
+    return path.endswith(device.address()) \
       and properties['Type'] == 'hfp'
 
   def __show_modem_properties(self, properties):
