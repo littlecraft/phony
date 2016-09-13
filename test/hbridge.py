36a5f86531864114b29521fe0c1b2aa8bcc832fd
Matthew Waddell <matt@littlecraft.io>
2016-09-12 19:02:15 -0700

    enable hbridge in test

--- test/hbridge.py
+++ test/hbridge.py
@@ -1,6 +1,7 @@
 import time
 from RPi import GPIO
 
+ringer_en = 16
 ringer_1 = 12
 ringer_2 = 13
 
@@ -9,7 +10,9 @@ def main():
 
   GPIO.setup(ringer_1, GPIO.OUT)
   GPIO.setup(ringer_2, GPIO.OUT)
+  GPIO.setup(ringer_en, GPIO.OUT)
 
+  GPIO.output(ringer_en, 1)
   GPIO.output(ringer_1, 0)
   GPIO.output(ringer_2, 0)
 
@@ -28,7 +31,8 @@ def main():
     v = not v
     c += 1
 
-    time.sleep(0.05)
+    #time.sleep(0.05)
+    time.sleep(2)
 
 if __name__ == '__main__':
   main()
