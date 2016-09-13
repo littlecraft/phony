5d48c3a5fb7f815284838b13509d0e226c805173
Matthew Waddell <matt@littlecraft.io>
2016-09-12 18:40:31 -0700

    Added hbridge gpio test driver

---
+++ test/hbridge.py
@@ -0,0 +1,34 @@
+import time
+from RPi import GPIO
+
+ringer_1 = 12
+ringer_2 = 13
+
+def main():
+  GPIO.setmode(GPIO.BCM)
+
+  GPIO.setup(ringer_1, GPIO.OUT)
+  GPIO.setup(ringer_2, GPIO.OUT)
+
+  GPIO.output(ringer_1, 0)
+  GPIO.output(ringer_2, 0)
+
+  c = 0
+  v = True
+
+  while True:
+    if v:
+      print '%d -> +' % c
+    else:
+      print '%d -> -' %c
+
+    GPIO.output(ringer_1, v)
+    GPIO.output(ringer_2, not v)
+
+    v = not v
+    c += 1
+
+    time.sleep(0.05)
+
+if __name__ == '__main__':
+  main()
